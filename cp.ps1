8c0cc306cc7431dca15448c0417ab7e907556bdb
Katman08 <52722158+Katman08@users.noreply.github.com>
2019-10-26 18:00:46 -0400

    v1.1

--- cp.ps1
+++ cp.ps1
@@ -1,14 +1,14 @@
-$ausers = @(user1,user2,user3)
+$ausers = @("user1", "user2", "user3")
 #authorized users^^^^
 
-$dusers = @(user4, user5, user6)
+$dusers = @("user4", "user5", "user6")
 #all users^^^^
 
-foreach $user in $dusers {
+foreach ($user in $dusers) {
 
 if ($ausers -match $user)
     {
-	foreach $user in $ausers {
+	foreach ($user in $ausers) {
 	Set-LocalUser -name $user -Password "Password@1" -PasswordNeverExpires false -UserMayChangePassword true
 	#changes passwords to "Password@1", disables passwordneverexpires, and allows changing password
 	}
