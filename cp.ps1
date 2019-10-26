d5f7c15560348fb9ecb4256ce24ce47d3fb743ec
Katman08 <52722158+Katman08@users.noreply.github.com>
2019-10-26 19:17:20 -0400

    v1.2
    -Disable Guest account
    -Disable Administrator account
    -Disable disallowed users
    -Change all passwords to "Password@1"
    -Disable password never expires for all users
    -Allow all users to change passwords
    -Enable windows firewall
    -Disable remote desktop

--- cp.ps1
+++ cp.ps1
@@ -4,6 +4,9 @@ $ausers = @("user1", "user2", "user3")
 $dusers = @("user4", "user5", "user6")
 #all users^^^^
 
+Disable-LocalUser -name "Guest"
+Disable-LocalUser -name "Administrator"
+
 foreach ($user in $dusers) {
 
 if ($ausers -match $user)
@@ -23,6 +26,7 @@ Disable-LocalUser -name $user
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 #enable Windows Firewall
 
-
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+#disable Remote Desktop
 
 
