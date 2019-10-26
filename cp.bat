0fa7d1aefa68de02a5bc5b6347be741c9d90f0c9
Katman08 <52722158+Katman08@users.noreply.github.com>
2019-10-26 18:37:23 -0400

    Password Policy

---
+++ cp.bat
@@ -0,0 +1,8 @@
+:password policy
+net accounts /minpwlen:10
+net accounts /maxpwage:30
+net accounts /minpwlen:10
+net accounts /UNIQUEPW:5
+net accounts /lockoutduration:30
+net accounts /lockoutthreshold:5
+net accounts /lockoutwindow:30
